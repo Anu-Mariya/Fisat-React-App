d3a3fe16bece48500651abc3038a0bb225b62e86
Anu-Mariya <anumariyavarghese0317@gmail.com>
2026-07-11 09:52:16 +0530

    Student UI completed

---
+++ src/components/StudentEntry.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StudentEntry = () => {
+  return (
+    <div>
+        <h1>
+            StudentEntry
+        </h1>
+    </div>
+  )
+}
+
+export default StudentEntry
